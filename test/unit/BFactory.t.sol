7a43d2734eefc14a21a93117f5ece3cefd48e309
0xAustrian <austrian@defi.sucks>
2024-05-03 10:09:15 -0300

    test: improve test naming

--- test/unit/BFactory.t.sol
+++ test/unit/BFactory.t.sol
@@ -20,7 +20,7 @@ contract BFactory_Unit_Constructor is Base {
   /**
    * @notice Test that the owner is set correctly
    */
-  function test_correctDeploy() public view {
+  function test_Deploy() public view {
     assertEq(owner, bFactory.getBLabs());
   }
 }
@@ -29,7 +29,7 @@ contract BFactory_Unit_IsBPool is Base {
   /**
    * @notice Test that a valid pool is present on the mapping
    */
-  function test_validPoolIsBPool() public {
+  function test_Returns_IsValidPool() public {
     BPool _pool = bFactory.newBPool();
     assertTrue(bFactory.isBPool(address(_pool)));
   }
@@ -37,7 +37,7 @@ contract BFactory_Unit_IsBPool is Base {
   /**
    * @notice Test that a invalid pool is not present on the mapping
    */
-  function test_invalidPoolIsNotBPool(address _randomPool) public view {
+  function test_Returns_IsInvalidPool(address _randomPool) public view {
     vm.assume(_randomPool != address(0));
     assertFalse(bFactory.isBPool(_randomPool));
   }
@@ -47,7 +47,7 @@ contract BFactory_Unit_NewBPool is Base {
   /**
    * @notice Test that the pool is set on the mapping
    */
-  function test_poolIsBPool() public {
+  function test_Set_Pool() public {
     BPool _pool = bFactory.newBPool();
     assertTrue(bFactory.isBPool(address(_pool)));
   }
@@ -55,7 +55,7 @@ contract BFactory_Unit_NewBPool is Base {
   /**
    * @notice Test that event is emitted
    */
-  function test_emitEvent() public {
+  function test_Emit_Log() public {
     vm.expectEmit(true, true, true, true);
     address _expectedPoolAddress = vm.computeCreateAddress(address(bFactory), 1);
     emit BFactory.LOG_NEW_POOL(owner, _expectedPoolAddress);
@@ -66,7 +66,7 @@ contract BFactory_Unit_NewBPool is Base {
   /**
    * @notice Test that msg.sender is set as the controller
    */
-  function test_controllerIsSet() public {
+  function test_Set_Controller() public {
     vm.prank(owner);
     BPool _pool = new BPool();
     assertEq(owner, _pool.getController());
@@ -75,7 +75,7 @@ contract BFactory_Unit_NewBPool is Base {
   /**
    * @notice Test that the pool address is returned
    */
-  function test_poolIsReturned() public {
+  function test_Returns_Pool() public {
     address _expectedPoolAddress = vm.computeCreateAddress(address(bFactory), 1);
     BPool _pool = bFactory.newBPool();
     assertEq(_expectedPoolAddress, address(_pool));
@@ -86,7 +86,7 @@ contract BFactory_Unit_GetBLabs is Base {
   /**
    * @notice Test that the correct owner is returned
    */
-  function test_correctOwner(address _randomDeployer) public {
+  function test_Set_Owner(address _randomDeployer) public {
     vm.prank(_randomDeployer);
     BFactory _bFactory = new BFactory();
     assertEq(_randomDeployer, _bFactory.getBLabs());
@@ -97,7 +97,7 @@ contract BFactory_Unit_SetBLabs is Base {
   /**
    * @notice Test that only the owner can set the BLabs
    */
-  function test_failIfNotBLabs(address _randomCaller) public {
+  function test_Revert_NotLabs(address _randomCaller) public {
     vm.assume(_randomCaller != owner);
     vm.expectRevert('ERR_NOT_BLABS');
     vm.prank(_randomCaller);
@@ -107,7 +107,7 @@ contract BFactory_Unit_SetBLabs is Base {
   /**
    * @notice Test that event is emitted
    */
-  function test_emitEvent(address _addressToSet) public {
+  function test_Emit_Log(address _addressToSet) public {
     vm.expectEmit(true, true, true, true);
     emit BFactory.LOG_BLABS(owner, _addressToSet);
     vm.prank(owner);
@@ -117,7 +117,7 @@ contract BFactory_Unit_SetBLabs is Base {
   /**
    * @notice Test that the BLabs is set correctly
    */
-  function test_setBLabs(address _addressToSet) public {
+  function test_Set_BLabs(address _addressToSet) public {
     vm.prank(owner);
     bFactory.setBLabs(_addressToSet);
     assertEq(_addressToSet, bFactory.getBLabs());
@@ -128,7 +128,7 @@ contract BFactory_Unit_Collect is Base {
   /**
    * @notice Test that only the owner can collect
    */
-  function test_failIfNotBLabs(address _randomCaller) public {
+  function test_Revert_NotLabs(address _randomCaller) public {
     vm.assume(_randomCaller != owner);
     vm.expectRevert('ERR_NOT_BLABS');
     vm.prank(_randomCaller);
@@ -138,7 +138,7 @@ contract BFactory_Unit_Collect is Base {
   /**
    * @notice Test that LP token `balanceOf` function is called
    */
-  function test_callBalanceOf(address _lpToken, uint _toCollect) public {
+  function test_Call_BalanceOf(address _lpToken, uint _toCollect) public {
     vm.assume(_lpToken != address(VM_ADDRESS));
     vm.mockCall(_lpToken, abi.encodeWithSelector(IERC20.balanceOf.selector, address(bFactory)), abi.encode(_toCollect));
     vm.mockCall(_lpToken, abi.encodeWithSelector(IERC20.transfer.selector, owner, _toCollect), abi.encode(true));
@@ -151,7 +151,7 @@ contract BFactory_Unit_Collect is Base {
   /**
    * @notice Test that LP token `transfer` function is called
    */
-  function test_callTransfer(address _lpToken, uint _toCollect) public {
+  function test_Call_Transfer(address _lpToken, uint _toCollect) public {
     vm.assume(_lpToken != address(VM_ADDRESS));
     vm.mockCall(_lpToken, abi.encodeWithSelector(IERC20.balanceOf.selector, address(bFactory)), abi.encode(_toCollect));
     vm.mockCall(_lpToken, abi.encodeWithSelector(IERC20.transfer.selector, owner, _toCollect), abi.encode(true));
@@ -164,7 +164,7 @@ contract BFactory_Unit_Collect is Base {
   /**
    * @notice Test that the function fail if the transfer failed
    */
-  function test_failIfTransferFailed(address _lpToken, uint _toCollect) public {
+  function test_Revert_TransferFailed(address _lpToken, uint _toCollect) public {
     vm.assume(_lpToken != address(VM_ADDRESS));
     vm.mockCall(_lpToken, abi.encodeWithSelector(IERC20.balanceOf.selector, address(bFactory)), abi.encode(_toCollect));
     vm.mockCall(_lpToken, abi.encodeWithSelector(IERC20.transfer.selector, owner, _toCollect), abi.encode(false));
