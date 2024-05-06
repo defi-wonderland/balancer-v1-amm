84e9c29bca8ef24534bddced1d6c0fb528f39b49
0xAustrian <austrian@defi.sucks>
2024-05-06 17:01:48 -0300

    test: add happy path for joinPool

--- test/unit/BPool.t.sol
+++ test/unit/BPool.t.sol
@@ -1,10 +1,68 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import {Test} from 'forge-std/Test.sol';
+import {BConst} from 'contracts/BConst.sol';
+import {BPool} from 'contracts/BPool.sol';
+import {IERC20} from 'contracts/BToken.sol';
+import {Test, console} from 'forge-std/Test.sol';
+import {LibString} from 'solmate/utils/LibString.sol';
 
-abstract contract Base is Test {
-  function setUp() public {}
+abstract contract Base is Test, BConst {
+  using LibString for *;
+
+  BPool public bPool;
+  address[8] public tokens;
+
+  function setUp() public {
+    bPool = new BPool();
+
+    // Create fake tokens
+    for (uint256 i = 0; i < tokens.length; i++) {
+      tokens[i] = makeAddr(i.toString());
+    }
+  }
+
+  function _preparePool(
+    BPool _pool,
+    address[8] memory _tokens,
+    uint256[8] memory _balance,
+    uint256[8] memory _denorm
+  ) internal {
+    // Create mocks
+    for (uint256 i = 0; i < _tokens.length; i++) {
+      vm.mockCall(_tokens[i], abi.encodeWithSelector(IERC20(_tokens[i]).transfer.selector), abi.encode(true));
+      vm.mockCall(_tokens[i], abi.encodeWithSelector(IERC20(_tokens[i]).transferFrom.selector), abi.encode(true));
+    }
+
+    // Set tokens
+    for (uint256 i = 0; i < _tokens.length; i++) {
+      vm.prank(_pool.getController());
+      _pool.bind(_tokens[i], _balance[i], _denorm[i]);
+    }
+
+    // Set public swap
+    vm.store(
+      address(_pool),
+      bytes32(uint256(6)),
+      bytes32(uint256(0x0000000000000000000000010000000000000000000000000000000000000000))
+    );
+    // Set finalize
+    vm.store(address(_pool), bytes32(uint256(8)), bytes32(uint256(1)));
+    // Set totalSupply
+    vm.store(address(_pool), bytes32(uint256(2)), bytes32(INIT_POOL_SUPPLY));
+  }
+
+  function _assumeHappyPath(
+    uint256[8] memory _balance,
+    uint256[8] memory _denorm
+  ) internal view returns (uint256[8] memory, uint256[8] memory) {
+    for (uint256 i = 0; i < _balance.length; i++) {
+      _balance[i] = bound(_balance[i], MIN_BALANCE, MIN_BALANCE * 1000000); // TODO: found a better max
+      _denorm[i] = bound(_denorm[i], MIN_WEIGHT, MAX_WEIGHT / 8); // Div by the max number of tokens
+    }
+
+    return (_balance, _denorm);
+  }
 }
 
 contract BPool_Unit_Constructor is Base {
@@ -252,6 +310,19 @@ contract BPool_Unit_GetSpotPriceSansFee is Base {
 }
 
 contract BPool_Unit_JoinPool is Base {
+  function test_HappyPath(uint256 _poolAmountOut, uint256[8] memory _balance, uint256[8] memory _denorm) public {
+    vm.assume(_poolAmountOut > INIT_POOL_SUPPLY);
+    vm.assume(_poolAmountOut < type(uint256).max / BONE);
+
+    uint256[] memory maxAmountsIn = new uint256[](tokens.length);
+    for (uint256 i = 0; i < tokens.length; i++) { maxAmountsIn[i] = type(uint256).max; } // Using max possible amounts
+
+    (uint256[8] memory __balance, uint256[8] memory __denorm) = _assumeHappyPath(_balance, _denorm);
+    _preparePool(bPool, tokens, __balance, __denorm);
+
+    bPool.joinPool(_poolAmountOut, maxAmountsIn);
+  }
+
   function test_Revert_NotFinalized() public view {}
 
   function test_Revert_MathApprox() public view {}
@@ -463,7 +534,7 @@ contract BPool_Unit_ExitswapPoolAmountOut is Base {
   function test_Revert_MaxOutRatio() public view {}
 
   function test_Revert_MathApprox() public view {}
-  
+
   function test_Revert_LimitIn() public view {}
 
   function test_Revert_Reentrancy() public view {}
