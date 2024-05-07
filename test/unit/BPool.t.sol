9fda7298f23fc69beaf237edc349ffaf268b685c
0xAustrian <austrian@defi.sucks>
2024-05-07 16:29:00 -0300

    test: wip

--- test/unit/BPool.t.sol
+++ test/unit/BPool.t.sol
@@ -11,7 +11,7 @@ abstract contract Base is Test, BConst {
   using LibString for *;
 
   BPool public bPool;
-  address[8] public tokens;
+  address[2] public tokens;
 
   function setUp() public {
     bPool = new BPool();
@@ -20,48 +20,6 @@ abstract contract Base is Test, BConst {
     for (uint256 i = 0; i < tokens.length; i++) {
       tokens[i] = makeAddr(i.toString());
     }
-  }
-
-  function _preparePool(
-    BPool _pool,
-    address[8] memory _tokens,
-    uint256[8] memory _balance,
-    uint256[8] memory _denorm
-  ) internal {
-    // Create mocks
-    for (uint256 i = 0; i < _tokens.length; i++) {
-      vm.mockCall(_tokens[i], abi.encodeWithSelector(IERC20(_tokens[i]).transfer.selector), abi.encode(true));
-      vm.mockCall(_tokens[i], abi.encodeWithSelector(IERC20(_tokens[i]).transferFrom.selector), abi.encode(true));
-    }
-
-    // Set tokens
-    for (uint256 i = 0; i < _tokens.length; i++) {
-      vm.prank(_pool.getController());
-      _pool.bind(_tokens[i], _balance[i], _denorm[i]);
-    }
-
-    // Set public swap
-    vm.store(
-      address(_pool),
-      bytes32(uint256(6)),
-      bytes32(uint256(0x0000000000000000000000010000000000000000000000000000000000000000))
-    );
-    // Set finalize
-    vm.store(address(_pool), bytes32(uint256(8)), bytes32(uint256(1)));
-    // Set totalSupply
-    vm.store(address(_pool), bytes32(uint256(2)), bytes32(INIT_POOL_SUPPLY));
-  }
-
-  function _assumeHappyPath(
-    uint256[8] memory _balance,
-    uint256[8] memory _denorm
-  ) internal view returns (uint256[8] memory, uint256[8] memory) {
-    for (uint256 i = 0; i < _balance.length; i++) {
-      _balance[i] = bound(_balance[i], MIN_BALANCE, MIN_BALANCE * 1000000); // TODO: found a better max
-      _denorm[i] = bound(_denorm[i], MIN_WEIGHT, MAX_WEIGHT / 8); // Div by the max number of tokens
-    }
-
-    return (_balance, _denorm);
   }
 }
 
@@ -310,17 +268,92 @@ contract BPool_Unit_GetSpotPriceSansFee is Base {
 }
 
 contract BPool_Unit_JoinPool is Base {
-  function test_HappyPath(uint256 _poolAmountOut, uint256[8] memory _balance, uint256[8] memory _denorm) public {
-    vm.assume(_poolAmountOut > INIT_POOL_SUPPLY);
-    vm.assume(_poolAmountOut < type(uint256).max / BONE);
 
+  struct FuzzScenario {
+    uint256 poolAmountOut;
+    uint256 balance0;
+    uint256 balance1;
+    // uint256 balance2;
+    // uint256 balance3;
+    // uint256 balance4;
+    // uint256 balance5;
+    // uint256 balance6;
+    // uint256 balance7;
+    // uint256[3] balance;
+  }
+
+  function _setValues(FuzzScenario memory _fuzz) internal {
+    // Create mocks
+    for (uint256 i = 0; i < tokens.length; i++) {
+      vm.mockCall(tokens[i], abi.encodeWithSelector(IERC20(tokens[i]).transfer.selector), abi.encode(true));
+      vm.mockCall(tokens[i], abi.encodeWithSelector(IERC20(tokens[i]).transferFrom.selector), abi.encode(true));
+    }
+
+    // Set tokens
+    bytes memory _arraySlot = abi.encode(9);
+    bytes32 _hashArraySlot = keccak256(_arraySlot);
+    vm.store(address(bPool), bytes32(_arraySlot), bytes32(tokens.length)); // write length
+    for (uint256 i = 0; i < tokens.length; i++) {
+      vm.store(address(bPool), bytes32(uint256(_hashArraySlot) + i), bytes32(abi.encode(tokens[i]))); // write token
+    }
+
+    // Set balances
+    for (uint256 i = 0; i < tokens.length; i++) {
+      bytes32 _slot = keccak256(abi.encode(tokens[i], 10)); // mapping is found at slot 10
+      vm.store(address(bPool), bytes32(uint256(_slot) + 0), bytes32(abi.encode(69))); // bound
+      vm.store(address(bPool), bytes32(uint256(_slot) + 3), bytes32(abi.encode(69))); // balance
+    }
+
+    // Set public swap
+    vm.store(
+      address(bPool),
+      bytes32(uint256(6)),
+      bytes32(uint256(0x0000000000000000000000010000000000000000000000000000000000000000))
+    );
+    // Set finalize
+    vm.store(address(bPool), bytes32(uint256(8)), bytes32(uint256(1)));
+    // Set totalSupply
+    vm.store(address(bPool), bytes32(uint256(2)), bytes32(INIT_POOL_SUPPLY));
+  }
+
+  function _assumeHappyPath(FuzzScenario memory _fuzz) internal view {
+    vm.assume(_fuzz.poolAmountOut > INIT_POOL_SUPPLY);
+    vm.assume(_fuzz.poolAmountOut < type(uint256).max / BONE);
+
+    // for (uint256 i = 0; i < _fuzz.balance.length; i++) {
+    //   vm.assume(_fuzz.balance[i] > MIN_BALANCE);
+    //   vm.assume(_fuzz.balance[i] < MIN_BALANCE * 1000000);
+    // }
+
+    vm.assume(_fuzz.balance0 > MIN_BALANCE);
+    vm.assume(_fuzz.balance0 < MIN_BALANCE * 1000000); // TODO: found a better max
+    vm.assume(_fuzz.balance1 > MIN_BALANCE);
+    vm.assume(_fuzz.balance1 < MIN_BALANCE * 1000000); // TODO: found a better max
+    // vm.assume(_fuzz.balance2 < MIN_BALANCE * 1000000); // TODO: found a better max
+    // vm.assume(_fuzz.balance2 > MIN_BALANCE);
+    // vm.assume(_fuzz.balance3 < MIN_BALANCE * 1000000); // TODO: found a better max
+    // vm.assume(_fuzz.balance3 > MIN_BALANCE);
+    // vm.assume(_fuzz.balance4 < MIN_BALANCE * 1000000); // TODO: found a better max
+    // vm.assume(_fuzz.balance4 > MIN_BALANCE);
+    // vm.assume(_fuzz.balance5 < MIN_BALANCE * 1000000); // TODO: found a better max
+    // vm.assume(_fuzz.balance5 > MIN_BALANCE);
+    // vm.assume(_fuzz.balance6 < MIN_BALANCE * 1000000); // TODO: found a better max
+    // vm.assume(_fuzz.balance6 > MIN_BALANCE);
+    // vm.assume(_fuzz.balance7 < MIN_BALANCE * 1000000); // TODO: found a better max
+    // vm.assume(_fuzz.balance7 > MIN_BALANCE);
+  }
+
+  modifier happyPath(FuzzScenario memory _fuzz) {
+    _assumeHappyPath(_fuzz);
+    _setValues(_fuzz);
+    _;
+  }
+
+  function test_HappyPath(FuzzScenario memory _fuzz) public happyPath(_fuzz) {
     uint256[] memory maxAmountsIn = new uint256[](tokens.length);
     for (uint256 i = 0; i < tokens.length; i++) { maxAmountsIn[i] = type(uint256).max; } // Using max possible amounts
 
-    (uint256[8] memory __balance, uint256[8] memory __denorm) = _assumeHappyPath(_balance, _denorm);
-    _preparePool(bPool, tokens, __balance, __denorm);
-
-    bPool.joinPool(_poolAmountOut, maxAmountsIn);
+    bPool.joinPool(_fuzz.poolAmountOut, maxAmountsIn);
   }
 
   function test_Revert_NotFinalized() public view {}
