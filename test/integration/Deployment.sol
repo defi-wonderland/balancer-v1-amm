e1896fd4d6b6510b81feb9ba66411af36a204104
Weißer Hase <wei3erHase@protonmail.com>
2024-06-04 17:11:49 +0200

    feat: adding deployment snapshots for factory and bPool

---
+++ test/integration/Deployment.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {BFactory} from 'contracts/BFactory.sol';
+
+import {GasSnapshot} from 'forge-gas-snapshot/GasSnapshot.sol';
+import {Test} from 'forge-std/Test.sol';
+
+contract DeploymentIntegrationTest is Test, GasSnapshot {
+  BFactory public factory;
+
+  function setUp() public {
+    factory = new BFactory();
+  }
+
+  function testFactoryDeployment() public {
+    snapStart('newBFactory');
+    new BFactory();
+    snapEnd();
+  }
+
+  function testDeployment() public {
+    snapStart('newBPool');
+    factory.newBPool();
+    snapEnd();
+  }
+}
