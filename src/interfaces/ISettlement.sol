bbc5c411c14aba7266abefc8df41826db17bc313
teddy <teddy@defi.sucks>
2024-06-07 16:44:56 -0300

    chore: import BCoWPool as-is from #26

---
+++ src/interfaces/ISettlement.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.25;
+
+interface ISettlement {
+  function domainSeparator() external view returns (bytes32);
+  function vaultRelayer() external view returns (address);
+}
