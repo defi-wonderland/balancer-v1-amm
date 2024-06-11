e0bdc34311f38bbff492d75569a14dbe20e73496
teddy <teddy@defi.sucks>
2024-06-11 19:49:48 -0300

    docs: add natspec for included interfaces

--- src/interfaces/ISettlement.sol
+++ src/interfaces/ISettlement.sol
@@ -2,6 +2,14 @@
 pragma solidity 0.8.25;
 
 interface ISettlement {
+  /**
+   * @return the domain separator for IERC1271 signature
+   * @dev is an immutable value, would not change on chain forks
+   */
   function domainSeparator() external view returns (bytes32);
+  /**
+   * @return the address that'll use the pool liquidity in CoWprotocol swaps
+   * @dev will transfer and transferFrom the pool. Has an infinite allowance.
+   */
   function vaultRelayer() external view returns (address);
 }
