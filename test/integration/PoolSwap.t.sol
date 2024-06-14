f6a97277eada29516af97e4dc9d1a77bda9a13da
Weißer Hase <wei3erHase@protonmail.com>
2024-06-14 15:21:19 +0200

    feat: adding inverse path on integration test

--- test/integration/PoolSwap.t.sol
+++ test/integration/PoolSwap.t.sol
@@ -17,6 +17,7 @@ abstract contract PoolSwapIntegrationTest is Test, GasSnapshot {
 
   address public lp = address(420);
   address public swapper = address(69);
+  address public swapperInverse = address(42_069);
 
   function setUp() public {
     tokenA = IERC20(address(deployMockERC20('TokenA', 'TKA', 18)));
@@ -26,6 +27,7 @@ abstract contract PoolSwapIntegrationTest is Test, GasSnapshot {
     deal(address(tokenB), address(lp), 100e18);
 
     deal(address(tokenA), address(swapper), 1e18);
+    deal(address(tokenB), address(swapperInverse), 1e18);
 
     factory = new BFactory();
 
@@ -56,10 +58,29 @@ abstract contract PoolSwapIntegrationTest is Test, GasSnapshot {
     // NOTE: no swap fees involved
     assertEq(tokenA.balanceOf(address(lp)), 100.5e18); // initial 100 + 0.5 tokenA
     // NOTE: hardcoded from test result
-    assertEq(tokenB.balanceOf(address(lp)), 99.903602078930850186e18); // initial 100 - ~0.09 tokenB
+    assertEq(tokenB.balanceOf(address(lp)), 99.903602078930850186e18); // initial 100 - ~0.1 tokenB
+  }
+
+  function testSimpleSwapInverse() public {
+    _makeSwapInverse();
+    assertEq(tokenA.balanceOf(address(swapperInverse)), 0.316986296266343639e18);
+    // NOTE: hardcoded from test result
+    assertEq(tokenB.balanceOf(address(swapperInverse)), 0.9e18);
+
+    vm.startPrank(lp);
+
+    uint256 lpBalance = pool.balanceOf(address(lp));
+    pool.exitPool(lpBalance, new uint256[](2));
+
+    // NOTE: no swap fees involved
+    assertEq(tokenA.balanceOf(address(lp)), 99.683013703733656361e18); // initial 100 - ~0.5 tokenA
+    // NOTE: hardcoded from test result
+    assertEq(tokenB.balanceOf(address(lp)), 100.1e18); // initial 100 + 0.1 tokenB
   }
 
   function _makeSwap() internal virtual;
+
+  function _makeSwapInverse() internal virtual;
 }
 
 contract DirectPoolSwapIntegrationTest is PoolSwapIntegrationTest {
@@ -70,6 +91,18 @@ contract DirectPoolSwapIntegrationTest is PoolSwapIntegrationTest {
     // swap 0.5 tokenA for tokenB
     snapStart('swapExactAmountIn');
     pool.swapExactAmountIn(address(tokenA), 0.5e18, address(tokenB), 0, type(uint256).max);
+    snapEnd();
+
+    vm.stopPrank();
+  }
+
+  function _makeSwapInverse() internal override {
+    vm.startPrank(swapperInverse);
+    tokenB.approve(address(pool), type(uint256).max);
+
+    // swap 0.1 tokenB for tokenA
+    snapStart('swapExactAmountInInverse');
+    pool.swapExactAmountIn(address(tokenB), 0.1e18, address(tokenA), 0, type(uint256).max);
     snapEnd();
 
     vm.stopPrank();
@@ -88,5 +121,9 @@ contract IndirectPoolSwapIntegrationTest is PoolSwapIntegrationTest {
     tokenA.transfer(address(pool), 0.5e18);
     tokenB.transferFrom(address(pool), address(swapper), 0.096397921069149814e18);
     vm.stopPrank();
+  }
+
+  function _makeSwapInverse() internal override {
+    vm.skip(true);
   }
 }
