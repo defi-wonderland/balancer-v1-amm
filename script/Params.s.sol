f98ed4eb53a778e983f2a89faa90739e72a4ab78
0xAustrian <austrian@defi.sucks>
2024-06-17 11:27:53 -0300

    fix: lint warning

--- script/Params.s.sol
+++ script/Params.s.sol
@@ -18,15 +18,15 @@ contract Params {
   mapping(uint256 _chainId => BCoWFactoryDeploymentParams _params) internal _bCoWFactoryDeploymentParams;
 
   /// @notice Settlement address
-  address internal _GPv2Settlement = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41;
+  address internal constant _GPV2_SETTLEMENT = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41;
 
   constructor() {
     // Mainnet
     _bFactoryDeploymentParams[1] = BFactoryDeploymentParams(address(this));
-    _bCoWFactoryDeploymentParams[1] = BCoWFactoryDeploymentParams(address(this), _GPv2Settlement);
+    _bCoWFactoryDeploymentParams[1] = BCoWFactoryDeploymentParams(address(this), _GPV2_SETTLEMENT);
 
     // Sepolia
     _bFactoryDeploymentParams[11_155_111] = BFactoryDeploymentParams(address(this));
-    _bCoWFactoryDeploymentParams[11_155_111] = BCoWFactoryDeploymentParams(address(this), _GPv2Settlement);
+    _bCoWFactoryDeploymentParams[11_155_111] = BCoWFactoryDeploymentParams(address(this), _GPV2_SETTLEMENT);
   }
 }
