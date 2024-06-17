10a610ddb297bfb8cc29971f0e166bd45f5184cf
0xAustrian <austrian@defi.sucks>
2024-06-17 10:08:45 -0300

    feat: add deployment scripts

--- script/DeployBFactory.s.sol
+++ script/DeployBFactory.s.sol
@@ -2,15 +2,14 @@
 pragma solidity 0.8.25;
 
 import {BFactory} from 'contracts/BFactory.sol';
+import {Script} from 'forge-std/Script.sol';
 import {Params} from 'script/Params.s.sol';
 
-import {Script} from 'forge-std/Script.sol';
-
-contract Deploy is Script, Params {
+contract DeployBFactory is Script, Params {
   function run() public {
-    DeploymentParams memory _params = _deploymentParams[block.chainid];
+    BFactoryDeploymentParams memory _params = _bFactoryDeploymentParams[block.chainid];
 
-    vm.startBroadcast();
+    vm.startBroadcast(_deployer[block.chainid]);
     BFactory bFactory = new BFactory();
     bFactory.setBLabs(_params.bLabs);
     vm.stopBroadcast();
--- script/Params.s.sol
+++ script/Params.s.sol
@@ -1,19 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
-contract Params {
-  struct DeploymentParams {
+import {Test} from 'forge-std/Test.sol';
+
+contract Params is Test {
+  struct BFactoryDeploymentParams {
     address bLabs;
   }
 
-  /// @notice Deployment parameters for each chain
-  mapping(uint256 _chainId => DeploymentParams _params) internal _deploymentParams;
+  struct BCoWFactoryDeploymentParams {
+    address bLabs;
+    address settlement;
+  }
+
+  /// @notice BFactory deployment parameters for each chain
+  mapping(uint256 _chainId => BFactoryDeploymentParams _params) internal _bFactoryDeploymentParams;
+
+  /// @notice BCoWFactory deployment parameters for each chain
+  mapping(uint256 _chainId => BCoWFactoryDeploymentParams _params) internal _bCoWFactoryDeploymentParams;
+
+  /// @notice Deployer address for each chain
+  mapping(uint256 _chainId => address _deployer) internal _deployer;
 
   constructor() {
+    _deployer[1] = vm.addr(uint256(vm.envBytes32('MAINNET_DEPLOYER_PK')));
+    _deployer[11_155_111] = vm.addr(uint256(vm.envBytes32('SEPOLIA_DEPLOYER_PK')));
+
     // Mainnet
-    _deploymentParams[1] = DeploymentParams(address(this));
+    _bFactoryDeploymentParams[1] = BFactoryDeploymentParams(address(this));
+    _bCoWFactoryDeploymentParams[1] = BCoWFactoryDeploymentParams(address(this), address(this));
 
     // Sepolia
-    _deploymentParams[11_155_111] = DeploymentParams(address(this));
+    _bFactoryDeploymentParams[11_155_111] = BFactoryDeploymentParams(address(this));
+    _bCoWFactoryDeploymentParams[11_155_111] = BCoWFactoryDeploymentParams(address(this), address(this));
   }
 }
