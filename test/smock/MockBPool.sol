228a4f4e96b53bb4c33b5f216cd386f9a5bbe005
teddy <teddy@defi.sucks>
2024-06-19 12:50:49 -0300

    refactor: manually mocked bpool is no longer necessary

--- test/smock/MockBPool.sol
+++ test/smock/MockBPool.sol
@@ -261,6 +261,25 @@ contract MockBPool is BPool, Test {
     vm.mockCall(address(this), abi.encodeWithSignature('getController()'), abi.encode(_returnParam0));
   }
 
+  function mock_call__setLock(bytes32 _value) public {
+    vm.mockCall(address(this), abi.encodeWithSignature('_setLock(bytes32)', _value), abi.encode());
+  }
+
+  function _setLock(bytes32 _value) internal override {
+    (bool _success, bytes memory _data) = address(this).call(abi.encodeWithSignature('_setLock(bytes32)', _value));
+
+    if (_success) return abi.decode(_data, ());
+    else return super._setLock(_value);
+  }
+
+  function call__setLock(bytes32 _value) public {
+    return _setLock(_value);
+  }
+
+  function expectCall__setLock(bytes32 _value) public {
+    vm.expectCall(address(this), abi.encodeWithSignature('_setLock(bytes32)', _value));
+  }
+
   function mock_call__pullUnderlying(address erc20, address from, uint256 amount) public {
     vm.mockCall(
       address(this),
@@ -347,24 +366,5 @@ contract MockBPool is BPool, Test {
 
   function expectCall__getLock() public {
     vm.expectCall(address(this), abi.encodeWithSignature('_getLock()'));
-  }
-
-  function mock_call__setLock(bytes32 _value) public {
-    vm.mockCall(address(this), abi.encodeWithSignature('_setLock(bytes32)', _value), abi.encode());
-  }
-
-  function _setLock(bytes32 _value) internal override {
-    (bool _success, bytes memory _data) = address(this).call(abi.encodeWithSignature('_setLock(bytes32)', _value));
-
-    if (_success) return abi.decode(_data, ());
-    else return super._setLock(_value);
-  }
-
-  function call__setLock(bytes32 _value) public {
-    return _setLock(_value);
-  }
-
-  function expectCall__setLock(bytes32 _value) public {
-    vm.expectCall(address(this), abi.encodeWithSignature('_setLock(bytes32)', _value));
   }
 }
--- test/unit/BPool.t.sol
+++ test/unit/BPool.t.sol
@@ -5,7 +5,7 @@ import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 import {BPool} from 'contracts/BPool.sol';
 import {IBPool} from 'interfaces/IBPool.sol';
-import {MockBPool} from 'test/manual-smock/MockBPool.sol';
+import {MockBPool} from 'test/smock/MockBPool.sol';
 
 import {BConst} from 'contracts/BConst.sol';
 import {BMath} from 'contracts/BMath.sol';
