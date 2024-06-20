c554c0f6e937428ee36070fe5349547861518673
Weißer Hase <wei3erHase@protonmail.com>
2024-06-20 12:21:54 +0200

    fix: missing files

---
+++ script/TestnetScript.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {IBPool} from 'contracts/BPool.sol';
+import {IBFactory} from 'interfaces/IBFactory.sol';
+import {IFaucet} from 'interfaces/IFaucet.sol';
+
+import {Script} from 'forge-std/Script.sol';
+import {Params} from 'script/Params.s.sol';
+
+contract TestnetScript is Script, Params {
+  /// @notice BFactory contract deployment address
+  IBFactory public bFactory = IBFactory(address(0x130289c2C29861a246BA45974E184A21F13843FC));
+
+  function run() public {
+    vm.startBroadcast();
+    (, address caller,) = vm.readCallers();
+
+    // NOTE: dripping can be called by anyone but only once a day (per address)
+    IFaucet(_SEPOLIA_FAUCET).drip(_SEPOLIA_BAL_TOKEN);
+    IFaucet(_SEPOLIA_FAUCET).drip(_SEPOLIA_DAI_TOKEN);
+    IFaucet(_SEPOLIA_FAUCET).drip(_SEPOLIA_USDC_TOKEN);
+
+    IBPool bPool = bFactory.newBPool();
+
+    IERC20(_SEPOLIA_BAL_TOKEN).approve(address(bPool), type(uint256).max);
+    IERC20(_SEPOLIA_DAI_TOKEN).approve(address(bPool), type(uint256).max);
+    IERC20(_SEPOLIA_USDC_TOKEN).approve(address(bPool), type(uint256).max);
+
+    bPool.bind(_SEPOLIA_BAL_TOKEN, 4e18, 1e18);
+    bPool.bind(_SEPOLIA_DAI_TOKEN, 1e18, 1e18);
+    bPool.bind(_SEPOLIA_USDC_TOKEN, 1e6, 1e18);
+
+    bPool.finalize();
+    vm.stopBroadcast();
+  }
+}
