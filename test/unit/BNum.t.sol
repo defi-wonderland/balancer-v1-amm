12f1e173575fb220ee8a728f53b83c873b26afa6
Weißer Hase <wei3erHase@protonmail.com>
2024-06-28 02:28:59 +0200

    test: syncing bnum unit tests to dev

--- test/unit/BNum.t.sol
+++ test/unit/BNum.t.sol
@@ -3,80 +3,36 @@ pragma solidity 0.8.25;
 
 import {BNum} from 'contracts/BNum.sol';
 import {Test} from 'forge-std/Test.sol';
+import {MockBNum} from 'test/smock/MockBNum.sol';
 
-// For test contract: expose the internal functions of BNum
-contract BNumExposed is BNum {
-  function btoiExposed(uint256 a) external pure returns (uint256) {
-    return btoi(a);
-  }
-
-  function bfloorExposed(uint256 a) external pure returns (uint256) {
-    return bfloor(a);
-  }
-
-  function baddExposed(uint256 a, uint256 b) external pure returns (uint256) {
-    return badd(a, b);
-  }
-
-  function bsubExposed(uint256 a, uint256 b) external pure returns (uint256) {
-    return bsub(a, b);
-  }
-
-  function bsubSignExposed(uint256 a, uint256 b) external pure returns (uint256, bool) {
-    return bsubSign(a, b);
-  }
-
-  function bmulExposed(uint256 a, uint256 b) external pure returns (uint256) {
-    return bmul(a, b);
-  }
-
-  function bdivExposed(uint256 a, uint256 b) external pure returns (uint256) {
-    return bdiv(a, b);
-  }
-
-  function bpowiExposed(uint256 a, uint256 n) external pure returns (uint256) {
-    return bpowi(a, n);
-  }
-
-  function bpowExposed(uint256 base, uint256 exp) external pure returns (uint256) {
-    return bpow(base, exp);
-  }
-}
-
-// Test contract
 contract BNumTest is Test {
-  BNumExposed bNum;
+  MockBNum bNum;
 
   uint256 BONE;
 
   function setUp() public {
-    bNum = new BNumExposed();
+    bNum = new MockBNum();
     BONE = bNum.BONE();
   }
 
   function test_BtoiWhenPassingZero() external {
-    // Action
-    uint256 _result = bNum.btoiExposed(0);
+    uint256 _result = bNum.call_btoi(0);
 
-    // Post Condition
     // it should return zero
     assertEq(_result, 0);
   }
 
   function test_BtoiWhenPassingBONE() external {
-    // Action
-    uint256 _result = bNum.btoiExposed(BONE);
+    uint256 _result = bNum.call_btoi(BONE);
 
     // it should return one
     assertEq(_result, 1);
   }
 
   function test_BtoiWhenPassingAValueLessThanBONE(uint256 _a) external {
-    // Preconditions
     _a = bound(_a, 0, BONE - 1);
 
-    // Action
-    uint256 _result = bNum.btoiExposed(_a);
+    uint256 _result = bNum.call_btoi(_a);
 
     // it should return zero
     assertEq(_result, 0);
@@ -88,22 +44,17 @@ contract BNumTest is Test {
   }
 
   function test_BfloorWhenPassingZero() external {
-    // Action
-    uint256 _result = bNum.bfloorExposed(0);
+    uint256 _result = bNum.call_bfloor(0);
 
-    // Post Condition
     // it should return zero
     assertEq(_result, 0);
   }
 
   function test_BfloorWhenPassingAValueLessThanBONE(uint256 _a) external {
-    // Preconditions
     _a = bound(_a, 0, BONE - 1);
 
-    // Action
-    uint256 _result = bNum.bfloorExposed(_a);
+    uint256 _result = bNum.call_bfloor(_a);
 
-    // Post Condition
     // it should return zero
     assertEq(_result, 0);
   }
@@ -114,38 +65,30 @@ contract BNumTest is Test {
   }
 
   function test_BaddWhenPassingZeroAndZero() external {
-    // Action
-    uint256 _result = bNum.baddExposed(0, 0);
+    uint256 _result = bNum.call_badd(0, 0);
 
-    // Post Condition
     // it should return zero
     assertEq(_result, 0);
   }
 
   function test_BaddRevertWhen_PassingAAsUint256MaxAndBNonZero(uint256 _b) external {
-    // Preconditions
     uint256 _a = type(uint256).max;
     _b = bound(_b, 1, type(uint256).max);
 
-    // Post Condition
     // it should revert
-    vm.expectRevert('ERR_ADD_OVERFLOW');
+    vm.expectRevert(BNum.BNum_AddOverflow.selector);
 
-    // Action
-    bNum.baddExposed(_a, _b);
+    bNum.call_badd(_a, _b);
   }
 
   function test_BaddRevertWhen_PassingBAsUint256MaxAndANonZero(uint256 _a) external {
-    // Preconditions
     _a = bound(_a, 1, type(uint256).max);
     uint256 _b = type(uint256).max;
 
-    // Post Condition
     // it should revert
-    vm.expectRevert('ERR_ADD_OVERFLOW');
+    vm.expectRevert(BNum.BNum_AddOverflow.selector);
 
-    // Action
-    bNum.baddExposed(_a, _b);
+    bNum.call_badd(_a, _b);
   }
 
   function test_BaddWhenPassingKnownValues() external {
@@ -154,16 +97,13 @@ contract BNumTest is Test {
   }
 
   function test_BsubRevertWhen_PassingALessThanB(uint256 _a, uint256 _b) external {
-    // Preconditions
     _a = bound(_a, 0, type(uint256).max - 1);
     _b = bound(_b, _a + 1, type(uint256).max);
 
-    // Post Condition
     // it should revert
-    vm.expectRevert('ERR_SUB_UNDERFLOW');
+    vm.expectRevert(BNum.BNum_SubUnderflow.selector);
 
-    // Action
-    bNum.bsubExposed(_a, _b);
+    bNum.call_bsub(_a, _b);
   }
 
   function test_BsubWhenPassingKnownValues() external {
@@ -172,86 +112,68 @@ contract BNumTest is Test {
   }
 
   function test_BsubSignWhenPassingZeroAndZero() external {
-    // Action
-    (uint256 _result, bool _flag) = bNum.bsubSignExposed(0, 0);
+    (uint256 _result, bool _flag) = bNum.call_bsubSign(0, 0);
 
-    // Post Condition
     // it should return zero and false
     assertEq(_result, 0);
     assertFalse(_flag);
   }
 
   function test_BsubSignWhenPassingALessThanB(uint256 _a, uint256 _b) external {
-    // Preconditions
     _a = bound(_a, 0, type(uint256).max - 1);
     _b = bound(_b, _a + 1, type(uint256).max);
 
-    // Action
-    (uint256 _result, bool _flag) = bNum.bsubSignExposed(_a, _b);
+    (uint256 _result, bool _flag) = bNum.call_bsubSign(_a, _b);
 
-    // Post Condition
     // it should return correct value and true
     assertEq(_result, _b - _a);
     assertTrue(_flag);
   }
 
   function test_BsubSignWhenPassingKnownValues() external {
-    // Action
-    (uint256 _result, bool _flag) = bNum.bsubSignExposed(5 * BONE, 3 * BONE);
+    (uint256 _result, bool _flag) = bNum.call_bsubSign(5 * BONE, 3 * BONE);
 
-    // Post Condition
     // it should return correct value
     assertEq(_result, 2 * BONE);
     assertFalse(_flag);
   }
 
   function test_BmulWhenPassingZeroAndZero() external {
-    // Action
-    uint256 _result = bNum.bmulExposed(0, 0);
+    uint256 _result = bNum.call_bmul(0, 0);
 
-    // Post Condition
     // it should return zero
     assertEq(_result, 0);
   }
 
   function test_BmulRevertWhen_PassingAAsUint256MaxAndBNonZero(uint256 _b) external {
-    // Preconditions
     uint256 _a = type(uint256).max;
     _b = bound(_b, 1, type(uint256).max);
 
-    // Post Condition
     // it should revert
-    vm.expectRevert('ERR_MUL_OVERFLOW');
+    vm.expectRevert(BNum.BNum_MulOverflow.selector);
 
-    // Action
-    bNum.bmulExposed(_a, _b);
+    bNum.call_bmul(_a, _b);
   }
 
   function test_BmulRevertWhen_PassingBAsUint256MaxAndANonZero(uint256 _a) external {
-    // Preconditions
     _a = bound(_a, 1, type(uint256).max);
     uint256 _b = type(uint256).max;
 
-    // Post Condition
     // it should revert
-    vm.expectRevert('ERR_MUL_OVERFLOW');
+    vm.expectRevert(BNum.BNum_MulOverflow.selector);
 
-    // Action
-    bNum.bmulExposed(_a, _b);
+    bNum.call_bmul(_a, _b);
   }
 
   function test_BmulWhenPassingAMulBTooBig(uint256 _a, uint256 _b) external {
-    // Preconditions
     _a = bound(_a, type(uint256).max / 2, type(uint256).max);
     _b = bound(_b, type(uint256).max / 2, type(uint256).max);
 
-    // Post Condition
     // it should revert
     //     a * b + BONE / 2 > uint256 max
-    vm.expectRevert('ERR_MUL_OVERFLOW');
+    vm.expectRevert(BNum.BNum_MulOverflow.selector);
 
-    // Action
-    bNum.bmulExposed(_a, _b);
+    bNum.call_bmul(_a, _b);
   }
 
   function test_BmulWhenPassingKnownValues() external {
@@ -260,46 +182,36 @@ contract BNumTest is Test {
   }
 
   function test_BdivRevertWhen_PassingBAsZero() external {
-    // Post Condition
     // it should revert
-    vm.expectRevert('ERR_DIV_ZERO');
+    vm.expectRevert(BNum.BNum_DivZero.selector);
 
-    // Action
-    bNum.bdivExposed(1, 0);
+    bNum.call_bdiv(1, 0);
   }
 
   function test_BdivRevertWhen_PassingAAsUint256Max() external {
-    // Post Condition
     // it should revert
-    vm.expectRevert('ERR_DIV_INTERNAL');
+    vm.expectRevert(BNum.BNum_DivInternal.selector);
 
-    // Action
-    bNum.bdivExposed(type(uint256).max, 1);
+    bNum.call_bdiv(type(uint256).max, 1);
   }
 
   function test_BdivWhenPassingAAndBTooBig() external {
-    // Preconditions
     uint256 _a = type(uint256).max;
     uint256 _b = BONE;
 
-    // Post Condition
     // it should revert
     //     a*BONE/b + b/2 > uint256 max
-    vm.expectRevert('ERR_DIV_INTERNAL');
+    vm.expectRevert(BNum.BNum_DivInternal.selector);
 
-    // Action
-    bNum.bdivExposed(_a, _b);
+    bNum.call_bdiv(_a, _b);
   }
 
   function test_BDivWhenFlooringToZero(uint256 _a, uint256 _b) external {
-    // Preconditions
     _a = bound(_a, 1, (type(uint256).max / (BONE * 2)) - 1);
     _b = bound(_b, (2 * BONE * _a) + 1, type(uint256).max);
 
-    // Action
-    uint256 _result = bNum.bdivExposed(_a, _b);
+    uint256 _result = bNum.call_bdiv(_a, _b);
 
-    // Post Condition
     // it should return 0
     assertEq(_result, 0);
   }
@@ -310,28 +222,22 @@ contract BNumTest is Test {
   }
 
   function test_BpowiWhenPassingAAsZero() external {
-    // Action
-    uint256 _result = bNum.bpowiExposed(0, 3);
+    uint256 _result = bNum.call_bpowi(0, 3);
 
-    // Post Condition
     // it should return zero
     assertEq(_result, 0);
   }
 
   function test_BpowiWhenPassingBAsZero() external {
-    // Action
-    uint256 _result = bNum.bpowiExposed(3 * BONE, 0);
+    uint256 _result = bNum.call_bpowi(3 * BONE, 0);
 
-    // Post Condition
     // it should return BONE
     assertEq(_result, BONE);
   }
 
   function test_BpowiWhenPassingAAsOne() external {
-    // Action
-    uint256 _result = bNum.bpowiExposed(BONE, 3);
+    uint256 _result = bNum.call_bpowi(BONE, 3);
 
-    // Post Condition
     // it should return BONE
     assertEq(_result, BONE);
   }
@@ -342,36 +248,28 @@ contract BNumTest is Test {
   }
 
   function test_BpowWhenPassingExponentAsZero() external {
-    // Action
-    uint256 _result = bNum.bpowExposed(BONE, 0);
+    uint256 _result = bNum.call_bpow(BONE, 0);
 
-    // Post Condition
     // it should return BONE
     assertEq(_result, BONE);
   }
 
   function test_BpowRevertWhen_PassingBaseLteThanMIN_BPOW_BASE(uint256 _base) external {
-    // Preconditions
     _base = bound(_base, 0, bNum.MIN_BPOW_BASE());
 
-    // Post Condition
     // it should revert
-    vm.expectRevert('ERR_BPOW_BASE_TOO_LOW');
+    vm.expectRevert(BNum.BNum_BPowBaseTooLow.selector);
 
-    // Action
-    bNum.bpowExposed(0, 3 * BONE);
+    bNum.call_bpow(0, 3 * BONE);
   }
 
   function test_BpowRevertWhen_PassingBaseGteMAX_BPOW_BASE(uint256 _base) external {
-    // Preconditions
     _base = bound(_base, bNum.MAX_BPOW_BASE(), type(uint256).max);
 
-    // Post Condition
     // it should revert
-    vm.expectRevert('ERR_BPOW_BASE_TOO_HIGH');
+    vm.expectRevert(BNum.BNum_BPowBaseTooHigh.selector);
 
-    // Action
-    bNum.bpowExposed(type(uint256).max, 3 * BONE);
+    bNum.call_bpow(type(uint256).max, 3 * BONE);
   }
 
   function test_BpowWhenPassingKnownValues() external {
