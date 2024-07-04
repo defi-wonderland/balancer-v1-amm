269ac01b3bf0e795bcd86327d32a85cb8c7326cd
Weißer Hase <wei3erHase@protonmail.com>
2024-07-04 19:59:57 +0200

    fix: natspec in IFaucet

--- src/interfaces/IFaucet.sol
+++ src/interfaces/IFaucet.sol
@@ -6,5 +6,9 @@ pragma solidity 0.8.25;
  * @notice External interface of Sepolia's Faucet contract.
  */
 interface IFaucet {
+  /**
+   * @notice Drips an amount of tokens to the caller.
+   * @param token The address of the token to drip.
+   */
   function drip(address token) external;
 }
