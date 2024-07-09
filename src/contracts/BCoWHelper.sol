68e8368b94cc2ebbe4526cc3c2157765ef9be114
Weißer Hase <wei3erHase@protonmail.com>
2024-07-09 16:19:38 +0200

    fix: lint run

--- src/contracts/BCoWHelper.sol
+++ src/contracts/BCoWHelper.sol
@@ -6,9 +6,10 @@ import {IBCoWPool} from 'interfaces/IBCoWPool.sol';
 import {IBFactory} from 'interfaces/IBFactory.sol';
 
 import {ICOWAMMPoolHelper} from '@cow-amm/interfaces/ICOWAMMPoolHelper.sol';
+
+import {IERC20} from '@cowprotocol/interfaces/IERC20.sol';
 import {GPv2Interaction} from '@cowprotocol/libraries/GPv2Interaction.sol';
 import {GPv2Order} from '@cowprotocol/libraries/GPv2Order.sol';
-import {IERC20} from '@cowprotocol/interfaces/IERC20.sol';
 
 // TODO: vendor from cow-amm
 import {GetTradeableOrder} from 'libraries/GetTradeableOrder.sol';
