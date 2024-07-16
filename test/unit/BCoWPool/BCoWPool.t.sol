284322a2786d2ce3a8e851c44a47ada78ea15261
teddy <teddy@defi.sucks>
2024-07-16 17:38:00 -0300

    test: btt tests for bcowpool.finalize

---
+++ test/unit/BCoWPool/BCoWPool.t.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {IERC20} from '@cowprotocol/interfaces/IERC20.sol';
+
+import {BCoWPoolBase} from './BCoWPoolBase.sol';
+
+import {IBCoWFactory} from 'interfaces/IBCoWFactory.sol';
+import {IBPool} from 'interfaces/IBPool.sol';
+
+contract BCoWPoolfinalize is BCoWPoolBase {
+  modifier whenPreconditionsAreMet() {
+    bCoWPool.set__tokens(tokens);
+    bCoWPool.set__records(tokens[0], IBPool.Record({bound: true, index: 0, denorm: tokenWeight}));
+    bCoWPool.set__records(tokens[1], IBPool.Record({bound: true, index: 1, denorm: tokenWeight}));
+    bCoWPool.mock_call__mintPoolShare(INIT_POOL_SUPPLY);
+    bCoWPool.mock_call__pushPoolShare(address(this), INIT_POOL_SUPPLY);
+
+    vm.mockCall(address(this), abi.encodeCall(IBCoWFactory.logBCoWPool, ()), abi.encode());
+
+    vm.mockCall(tokens[0], abi.encodeCall(IERC20.approve, (vaultRelayer, type(uint256).max)), abi.encode(true));
+    vm.mockCall(tokens[1], abi.encodeCall(IERC20.approve, (vaultRelayer, type(uint256).max)), abi.encode(true));
+    _;
+  }
+
+  function test_WhenPreconditionsAreMet() external whenPreconditionsAreMet {
+    // it calls approve on every bound token
+    vm.expectCall(tokens[0], abi.encodeCall(IERC20.approve, (vaultRelayer, type(uint256).max)));
+    vm.expectCall(tokens[1], abi.encodeCall(IERC20.approve, (vaultRelayer, type(uint256).max)));
+    // it calls logBCoWPool on the factory
+    vm.expectCall(address(this), abi.encodeCall(IBCoWFactory.logBCoWPool, ()));
+    bCoWPool.finalize();
+  }
+
+  function test_WhenFactorysLogBCoWPoolDoesNotRevert() external whenPreconditionsAreMet {
+    // it returns
+    bCoWPool.finalize();
+  }
+
+  function test_WhenFactorysLogBCoWPoolReverts(bytes memory revertData) external whenPreconditionsAreMet {
+    vm.mockCallRevert(address(this), abi.encodeCall(IBCoWFactory.logBCoWPool, ()), revertData);
+    // it emits a COWAMMPoolCreated event
+    vm.expectEmit(address(bCoWPool));
+    emit IBCoWFactory.COWAMMPoolCreated(address(bCoWPool));
+    bCoWPool.finalize();
+  }
+}
